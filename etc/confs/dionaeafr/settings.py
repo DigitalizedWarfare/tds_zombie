aaf6e1abd2423464c6dfca4607fa31b9e2676d80
Christian <christian@digitalizedwarfare.com>
2015-02-07 11:28:36 -0500

    Updated Installer

--- etc/confs/dionaeafr/settings.py
+++ etc/confs/dionaeafr/settings.py
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/var/dionaea/logsql.sqlite',
+        'NAME': '/opt/dionaea/var/dionaea/logsql.sqlite',
         'USER': '',  # Not used with sqlite3.
         'PASSWORD': '',  # Not used with sqlite3.
         'HOST': '',  # Set to empty string for localhost. Not used with sqlite3.
