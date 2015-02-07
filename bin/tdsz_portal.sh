3a5a589e81422c11f57db877479bf2df2c737347
Christian <christian@digitalizedwarfare.com>
2015-02-07 07:06:39 -0500

    Updated Installer

--- bin/tdsz_portal.sh
+++ bin/tdsz_portal.sh
@@ -139,9 +139,55 @@ install_dionaea(){
 	apt-get update
 	read -p "System Updated : Press [Enter] key to continue..." fackEnterKey
 	clear
-	echo "We need to Install GIT : "
-	apt-get install git -y
-	read -p "GIT Installed : Press [Enter] key to continue..." fackEnterKey
+	echo "We need to Install some Python Files : "
+	apt-get install python-pip python-netaddr build-essential python-dev -y
+	read -p "Now We ned to nstall some PIP Files : Press [Enter] key to continue..." fackEnterKey
+	pip install Django
+	pip install pygeoip
+	pip install django-pagination
+	pip install django-tables2
+	pip install django-compressor
+	pip install django-htmlmin
+	pip install django-filter
+	
+	cd /opt/
+	wget https://github.com/benjiec/django-tables2-simplefilter/archive/master.zip -O django-tables2-simplefilter.zip
+	unzip django-tables2-simplefilter.zip
+	mv django-tables2-simplefilter-master/ django-tables2-simplefilter/
+	cd django-tables2-simplefilter/
+	python setup.py install
+	
+	cd /opt/
+	git clone https://github.com/bro/pysubnettree.git
+	cd pysubnettree/
+	python setup.py install
+	
+	cd /opt/
+	wget http://nodejs.org/dist/v0.12.0/node-v0.12.0-linux-x64.tar.gz
+	tar xzvf node-v0.12.0-linux-x64.tar.gz
+	cd node-v0.12.0-linux-x64
+	./configure
+	make
+	make install
+	
+	npm install -g less
+	
+	cd /opt/
+	wget https://github.com/RootingPuntoEs/DionaeaFR/archive/master.zip -O DionaeaFR.zip
+	unzip DionaeaFR.zip
+	mv DionaeaFR-master/ DionaeaFR
+	
+	cd /opt/
+	wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
+	wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
+	gunzip GeoLiteCity.dat.gz
+	gunzip GeoIP.dat.gz
+	mv GeoIP.dat DionaeaFR/DionaeaFR/static
+	mv GeoLiteCity.dat DionaeaFR/DionaeaFR/static
+	
+	apt-get install python-pip python-netaddr
+	apt-get install unzip sqlite
+	read -p "DionaeaFR Installed : Press [Enter] key to continue..." fackEnterKey
 	clear
 }
 install_honeyd(){
