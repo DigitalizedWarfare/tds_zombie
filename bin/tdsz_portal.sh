cb41c14d28c5ace25b0eeae8a4454d6c888bde66
Christian <christian@digitalizedwarfare.com>
2015-02-07 10:26:59 -0500

    Structure Added

--- bin/tdsz_portal.sh
+++ bin/tdsz_portal.sh
@@ -244,23 +244,23 @@ install_dionaea(){
 	read -p "Check Install..." fackEnterKey
 	git clone git://git.carnivore.it/dionaea.git dionaea
 	cd dionaea 
-	autoreconf -vi
-	./configure –with-lcfg-include=/opt/dionaea/include/ \
-	–with-lcfg-lib=/opt/dionaea/lib/ \
-	–with-python=/opt/dionaea/bin/python3.2 \
-	–with-cython-dir=/opt/dionaea/bin \
-	–with-udns-include=/opt/dionaea/include/ \
-	–with-udns-lib=/opt/dionaea/lib/ \
-	–with-emu-include=/opt/dionaea/include/ \
-	–with-emu-lib=/opt/dionaea/lib/ \
-	–with-gc-include=/usr/include/gc \
-	–with-ev-include=/opt/dionaea/include \
-	–with-ev-lib=/opt/dionaea/lib \
-	–with-nl-include=/opt/dionaea/include \
-	–with-nl-lib=/opt/dionaea/lib/ \
-	–with-curl-config=/usr/bin/ \
-	–with-pcap-include=/opt/dionaea/include \
-	–with-pcap-lib=/opt/dionaea/lib/
+	autoreconf -v -i
+	./configure --with-lcfg-include=/opt/dionaea/include/ \
+	--with-lcfg-lib=/opt/dionaea/lib/ \
+	--with-python=/opt/dionaea/bin/python3.2 \
+	--with-cython-dir=/opt/dionaea/bin \
+	--with-udns-include=/opt/dionaea/include/ \
+	--with-udns-lib=/opt/dionaea/lib/ \
+	--with-emu-include=/opt/dionaea/include/ \
+	--with-emu-lib=/opt/dionaea/lib/ \
+	--with-gc-include=/usr/include/gc \
+	--with-ev-include=/opt/dionaea/include \
+	--with-ev-lib=/opt/dionaea/lib \
+	--with-nl-include=/opt/dionaea/include \
+	--with-nl-lib=/opt/dionaea/lib/ \
+	--with-curl-config=/usr/bin/ \
+	--with-pcap-include=/opt/dionaea/include \
+	--with-pcap-lib=/opt/dionaea/lib/
 	make
 	make install
 	
